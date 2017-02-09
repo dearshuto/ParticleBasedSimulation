2613a67fa48c9d75f3865f7c9231bb1cddaf9c36
Shuto Shikama <shuto.shikama@fj.ics.keio.ac.jp>
2017-02-09 21:50:00 +0900

    [modify] Refactor

--- include/particle_based_simulation/simulation/algorithm/general/particle_algorithm.hpp
+++ include/particle_based_simulation/simulation/algorithm/general/particle_algorithm.hpp
@@ -9,12 +9,11 @@
 #ifndef particle_algorithm_hpp
 #define particle_algorithm_hpp
 
-#include "particle_based_simulation/additional/povray/povray_output.hpp"
-
 #include "algorithm.hpp"
 
 namespace fj {
     class OverlapParticleWorld;
+    class POVRayOutput;
     class ParticleAlgorithm;
 }
 
@@ -22,27 +21,13 @@ namespace fj {
 class fj::ParticleAlgorithm : public fj::Algorithm
 {
 public:
-    /// fj::ParticleAlgorithm の情報をプロファイルするシステム一覧
-    enum class Profile : uint8_t
-    {
-        kPOVRayOutput,
-    };
-public:
     ParticleAlgorithm() = default;
     virtual~ParticleAlgorithm() = default;
     
     
-    /** POV-Ray形式でシーンを出力するシステムを作成する.
-     * @return 作成したシステムへのポインタ
-     * @todo 複数回呼び出すと, 同じ役割のインスタンスが作成されてしまう問題を解決する. */
-    std::weak_ptr<fj::POVRayOutput> setupPOVRaySceneOutputSystem()
-    {
-        std::unique_ptr<fj::POVRayOutput> povray{new fj::POVRayOutput(getOverlapParticleWorld())};
-        std::weak_ptr<fj::AdditionalProcedure> result = addProfileSystem(std::move(povray));
-        auto returnPtr = std::static_pointer_cast<fj::POVRayOutput>(result.lock());
-        return std::weak_ptr<fj::POVRayOutput>{returnPtr};
-    }
-    
+    /** POV-Ray形式でシーンを出力するシステムを作成する. この関数を複数回呼んだときの挙動は未定義.
+     * @return 作成したシステムへのポインタ. */
+    std::weak_ptr<fj::POVRayOutput> setupPOVRaySceneOutputSystem();
     
     virtual const fj::OverlapParticleWorld& getOverlapParticleWorld()const = 0;
     virtual fj::OverlapParticleWorld* getOverlapParticleWorldPtr() = 0;
